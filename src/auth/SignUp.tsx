a7ac4632f98a28bff944f18ecd58bd497e39e1c3
adiaber1994 <106983011+adiaber1994@users.noreply.github.com>
2023-06-24 18:03:36 +0300

    sign up

--- src/auth/SignUp.tsx
+++ src/auth/SignUp.tsx
@@ -1,34 +1,75 @@
 import Title from "../component/Title";
 
-function signup() {
+function Signup() {
   return (
     <>
+  
       <Title mainText={"REGISTER"} />
 
-      <div>
-        <div className="row">
-          <div className="col">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="First name"
-              aria-label="First name"
-            />
-          </div>
-          <div className="col">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Last name"
-              aria-label="Last name"
-            />
-          </div>
-        </div>
-      </div>
-
-      <button className="btn btn-secondary"> Cancel</button>
-    </>
+     
+     <form className="row g-3">
+        <div className="col-md-6">
+           <label htmlFor="" className="form-label">First Name</label>
+            <input type="name" className="form-control" id=""/>
+         </div>
+  <div className="col-md-6">
+    <label htmlFor="" className="form-label">Middle Name</label>
+    <input type="name" className="form-control" id=""/>
+  </div>
+  <div className="col-md-6">
+    <label htmlFor="" className="form-label">Last Name</label>
+    <input type="name" className="form-control" id=""/>
+  </div>
+  <div className="col-md-6">
+    <label htmlFor="" className="form-label">Phone</label>
+    <input type="phone" className="form-control" id=""/>
+  </div>
+  <div className="col-md-6">
+    <label htmlFor="" className="form-label">Email</label>
+    <input type="email" className="form-control" id="inputPassword4"/>
+  </div>
+  <div className="col-md-6">
+    <label htmlFor="inputPassword4" className="form-label">Password</label>
+    <input type="password" className="form-control" id="inputPassword4"/>
+  </div>
+  <div className="col-12">
+    <label htmlFor="inputAddress" className="form-label">Address</label>
+    <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St"/>
+  </div>
+  <div className="col-12">
+    <label htmlFor="inputAddress2" className="form-label">Address 2</label>
+    <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"/>
+  </div>
+  <div className="col-md-6">
+    <label htmlFor="inputCity" className="form-label">City</label>
+    <input type="text" className="form-control" id="inputCity"/>
+  </div>
+  <div className="col-md-4">
+    <label htmlFor="inputState" className="form-label">State</label>
+    <select id="inputState" className="form-select">
+      <option selected>Choose...</option>
+      <option>...</option>
+    </select>
+  </div>
+  <div className="col-md-2">
+    <label htmlFor="inputZip" className="form-label">Zip</label>
+    <input type="text" className="form-control" id="inputZip"/>
+  </div>
+  <div className="col-12">
+    <div className="form-check">
+      <input className="form-check-input" type="checkbox" id="gridCheck"/>
+      <label className="form-check-label" htmlFor="gridCheck">
+        Check me out
+      </label>
+    </div>
+     </div>
+     <div className="col-12">
+    <button type="submit" className="btn btn-primary">Submmit</button>
+     </div>
+    </form>
+  </>
+  
   );
 }
 
-export default signup;
+export default Signup;
